8bff62222005884f54ac9dfe6952a62647719092
Andre Ortiz <hello@andreortiz.com>
2018-04-05 17:13:42 -0500

    added madlib game

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,11 +1,111 @@
 import React, { Component } from 'react';
+import storyJSON from '../lib/story.json'
 
 export default class Example extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      responses: [],
+      currentResponse: '',
+      questions: storyJSON.story,
+      currentState: 0,
+      questionCount: storyJSON.story.length
+    }
+
+    this.submitResponse = this.submitResponse.bind(this);
+    this.updateResponse = this.updateResponse.bind(this);
+    this.resetGame = this.resetGame.bind(this);
+
+    this._ui_render_form = this._ui_render_form.bind(this);
+    this._ui_render_result = this._ui_render_result.bind(this);
+    this._ui_render_actions = this._ui_render_actions.bind(this);
+  }
+
+  updateResponse (event) {
+    this.setState({
+      currentResponse: event.target.value
+    })
+  }
+
+  submitResponse(){
+    var tempArray = this.state.responses
+    tempArray.push(this.state.currentResponse)
+
+    this.setState({
+      responses: tempArray,
+      currentResponse: '',
+      currentState: this.state.currentState+1,
+    })
+  }
+
+  resetGame () {
+    this.setState({
+      responses: [],
+      currentResponse: '',
+      currentState: 0
+    })
+  }
+
+  _ui_render_form(){
+    var self = this
+    return this.state.questions.map(function(q, index){
+      return (
+        <div className={self.state.currentState === index ? "active panel-forms" : "panel-forms"} key={index}>
+          <div className="field" data-step={index}>
+            <input type="text" className="input-field" value={self.state.currentResponse} onChange={self.updateResponse}/>
+            <span className="instruction">{q.type}</span>
+          </div>
+        </div>
+        )
+    })
+  }
+
+  _ui_render_result(){
+    var self = this
+    return (
+    <div className="story-result">
+      <p className="lead">
+        Andre Ortiz is a <span>{ this.state.responses[0] }</span> dude living in Austin, TX. He's always learning new skills like <span>{ this.state.responses[1] }</span> and <span>{ this.state.responses[2] }</span> (that new javascript framework). Mostly, he enjoys designing and building software with <span>{ this.state.responses[3] }</span> people and becoming better at his craft. Andre is the type of <span>{ this.state.responses[4] }</span> person you'd want on your team.
+      </p>
+    </div>
+    )
+  }
+
+  _ui_render_actions(){
+    if (this.state.currentState >= this.state.questionCount) {
+      return (
+        <div className="panel-actions">
+          <button type="button" className="next-btn" onClick={this.resetGame.bind(this)}>Do it again</button>
+        </div>
+      )
+    } else {
+      return (
+        <div className="panel-actions">
+          <button type="button" className="next-btn" onClick={this.submitResponse.bind(this)}>Next</button>
+          <button type="button" className="auto-gen-btn">Auto-Generate</button>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div className="example-container">
-        example here
+        <div className="container">
+
+          <h2>Honest to goodness, working software</h2>
+          <p className="lead">You like Mad Libs&trade;? Of course you do. Here's an example of working components, state management and fun.</p>
+
+          <div className="panel">
+
+            <div className="panel-container">
+              { this.state.currentState >= this.state.questionCount ? this._ui_render_result() : this._ui_render_form() }
+            </div>
+
+            { this._ui_render_actions() }
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
--- src/components/SkillMeter.js
+++ src/components/SkillMeter.js
@@ -6,14 +6,14 @@ export default class SkillMeter extends Component {
       <div className="skill-meter-component">
         <svg width="100%" height="100%" viewBox="0 0 40 40" className={"donut " + this.props.color}>
           <circle className="donut-hole" cx="21" cy="21" r="15.91549430918954"></circle>
-          <circle className="donut-ring" cx="21" cy="21" r="15.91549430918954" stroke-width="2"></circle>
-          <circle className="donut-segment" cx="21" cy="21" r="15.91549430918954" stroke-width="2" stroke-dasharray={ this.props.amount } stroke-dashoffset="25"></circle>
+          <circle className="donut-ring" cx="21" cy="21" r="15.91549430918954" strokeWidth="2"></circle>
+          <circle className="donut-segment" cx="21" cy="21" r="15.91549430918954" strokeWidth="2" strokeDasharray={ this.props.amount } strokeDashoffset="25"></circle>
 
 
-          <text x="14.5" y="24" font-family="Roboto Slab" font-weight="bold" font-size="9" stroke="none" fill="black">
+          <text x="14.5" y="24" fontFamily="Roboto Slab" fontWeight="bold" fontSize="9" stroke="none" fill="black">
             {this.props.amount[0] + this.props.amount[1]}
           </text>
-          <text x="24.5" y="24" font-family="Roboto Slab" font-weight="normal" font-size="6" stroke="none" fill="black">
+          <text x="24.5" y="24" fontFamily="Roboto Slab" fontWeight="normal" fontSize="6" stroke="none" fill="black">
             {"%"}
           </text>
         </svg>
